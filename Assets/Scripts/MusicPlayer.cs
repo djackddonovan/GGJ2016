4e5d9dcf55c74bcbf100740aa311beb20f75fe6f
Djimy <djimy01@hotmail.fr>
2016-01-30 19:45:58 +0100

    more progress with sounds

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	AudioSource src;
+	SoundManager mgr;
+
+	void Awake () {
+		src = GetComponent<AudioSource> ();
+		mgr = GetComponent<SoundManager> ();
+	}
+
+	public void PlayMusic (string musicName) {
+		mgr.PlaySound (src, musicName, false, true);
+	}
+
+}
